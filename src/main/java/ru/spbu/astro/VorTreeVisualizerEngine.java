c4a6a86ee342c66d29ff801eac9181fe7fc0881c
Fedor Amosov <amosov.f@mail.ru>
2014-02-11 03:02:09 +0400

    Syntax refactoring

--- src/main/java/ru/spbu/astro/VorTreeVisualizerEngine.java
+++ src/main/java/ru/spbu/astro/VorTreeVisualizerEngine.java
@@ -62,17 +62,5 @@ public class VorTreeVisualizerEngine {
             plot.addPlot(Plotter.linePlot("count", Color.BLUE, NativeDelaunayGraphBuilder.count));
             new FrameView(plot);
         }
-
-        /*
-        for (int m = 30; m < 50; ++m) {
-            double average = 0;
-            for (int t = 0; t < 10; ++t) {
-                Binder binder = new Binder(1000, m, SIZE_X, SIZE_Y);
-                average += binder.getRate();
-            }
-            average /= 10;
-            System.out.println(m + " " + average);
-        }
-        */
     }
 }
